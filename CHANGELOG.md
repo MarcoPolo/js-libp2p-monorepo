04c1ebde11d1e84ab45609639c099b7157f4bc5b
Marco Munizaga <git@marcopolo.io>
2023-03-07 16:57:08 -0800

    Squashed 'transports/webtransport/' content from commit 63f0c33b
    git-subtree-dir: transports/webtransport
    git-subtree-split: 63f0c33bc2b876c11ec756e132bb679f32f46245

---
+++ CHANGELOG.md
@@ -0,0 +1,70 @@
+## [1.0.7](https://github.com/libp2p/js-libp2p-webtransport/compare/v1.0.6...v1.0.7) (2023-01-12)
+
+
+### Dependencies
+
+* Update deps and add quic-v1 support ([#44](https://github.com/libp2p/js-libp2p-webtransport/issues/44)) ([d1613b1](https://github.com/libp2p/js-libp2p-webtransport/commit/d1613b10c1c8164fadcbe9a28175b7f0099d1645)), closes [#35](https://github.com/libp2p/js-libp2p-webtransport/issues/35)
+
+
+### Documentation
+
+* update project config to publish api docs ([#45](https://github.com/libp2p/js-libp2p-webtransport/issues/45)) ([bdbf402](https://github.com/libp2p/js-libp2p-webtransport/commit/bdbf4025b5f80d6cc8af49596da09386538dc791))
+
+## [1.0.6](https://github.com/libp2p/js-libp2p-webtransport/compare/v1.0.5...v1.0.6) (2022-12-06)
+
+
+### Bug Fixes
+
+* Make a fix release with [#32](https://github.com/libp2p/js-libp2p-webtransport/issues/32) ([#34](https://github.com/libp2p/js-libp2p-webtransport/issues/34)) ([66a38f6](https://github.com/libp2p/js-libp2p-webtransport/commit/66a38f6e452e72042ad10ef8521544d8b5afecff))
+
+## [1.0.5](https://github.com/libp2p/js-libp2p-webtransport/compare/v1.0.4...v1.0.5) (2022-11-16)
+
+
+### Bug Fixes
+
+* Close stream after sink ([#23](https://github.com/libp2p/js-libp2p-webtransport/issues/23)) ([a95720c](https://github.com/libp2p/js-libp2p-webtransport/commit/a95720c367c8061ae45b4ae4bc4180e3ceea61cc))
+
+## [1.0.4](https://github.com/libp2p/js-libp2p-webtransport/compare/v1.0.3...v1.0.4) (2022-11-01)
+
+
+### Documentation
+
+* Use textarea for multiaddr input in example ([#24](https://github.com/libp2p/js-libp2p-webtransport/issues/24)) ([14ce351](https://github.com/libp2p/js-libp2p-webtransport/commit/14ce351375dabb31df948005b20acff56acc483a))
+
+## [1.0.3](https://github.com/libp2p/js-libp2p-webtransport/compare/v1.0.2...v1.0.3) (2022-10-18)
+
+
+### Documentation
+
+* add fetch-file-from-kubo example ([#12](https://github.com/libp2p/js-libp2p-webtransport/issues/12)) ([4a8f2f3](https://github.com/libp2p/js-libp2p-webtransport/commit/4a8f2f3eb4fdede1510aa2808d4c9a30d7ae86bf))
+
+## [1.0.2](https://github.com/libp2p/js-libp2p-webtransport/compare/v1.0.1...v1.0.2) (2022-10-17)
+
+
+### Bug Fixes
+
+* update project, remove @libp2p/components and unused deps ([#20](https://github.com/libp2p/js-libp2p-webtransport/issues/20)) ([568638e](https://github.com/libp2p/js-libp2p-webtransport/commit/568638e9fddc57726547e9147647af468a28bf51))
+
+## [1.0.1](https://github.com/libp2p/js-libp2p-webtransport/compare/v1.0.0...v1.0.1) (2022-10-12)
+
+
+### Dependencies
+
+* bump multiformats from 9.9.0 to 10.0.0 ([c3f7d22](https://github.com/libp2p/js-libp2p-webtransport/commit/c3f7d220969de6ec8a632738f760ab11388ef3e7))
+* **dev:** bump @libp2p/interface-transport-compliance-tests ([62c8e6b](https://github.com/libp2p/js-libp2p-webtransport/commit/62c8e6b3c18959d7416767d307a5ebaac8c19ae8))
+* **dev:** bump protons from 5.1.0 to 6.0.0 ([03f7f33](https://github.com/libp2p/js-libp2p-webtransport/commit/03f7f33ba5561771746f1f1cfff7421da36c5889))
+
+## 1.0.0 (2022-10-12)
+
+
+### Trivial Changes
+
+* Update .github/workflows/stale.yml [skip ci] ([cc208ac](https://github.com/libp2p/js-libp2p-webtransport/commit/cc208acc1c3459e5ec5f230927bfe5dc6e175f39))
+* use rc version of libp2p ([bdc9dfb](https://github.com/libp2p/js-libp2p-webtransport/commit/bdc9dfb63f9853a38bc3b6999a1f986bff116dda))
+
+
+### Dependencies
+
+* bump protons-runtime from 3.1.0 to 4.0.1 ([a7ef395](https://github.com/libp2p/js-libp2p-webtransport/commit/a7ef3959d024813caa327afdd502d5bcb91a15e3))
+* **dev:** bump @libp2p/interface-mocks from 4.0.3 to 7.0.1 ([85a492d](https://github.com/libp2p/js-libp2p-webtransport/commit/85a492da5b8df76d710dd21dd4b8bf59df4e1184))
+* **dev:** bump uint8arrays from 3.1.1 to 4.0.2 ([cb554e8](https://github.com/libp2p/js-libp2p-webtransport/commit/cb554e8dbb19a6ec5b085307f4c04c04ae313d2d))
---
+++ test/browser.ts
@@ -0,0 +1,148 @@
+/* eslint-disable no-console */
+/* eslint-env mocha */
+
+import { expect } from 'aegir/chai'
+import { multiaddr } from '@multiformats/multiaddr'
+import { noise } from '@chainsafe/libp2p-noise'
+import { webTransport, isSubset } from '../src/index'
+import { createLibp2p } from 'libp2p'
+
+declare global {
+  interface Window {
+    WebTransport: any
+  }
+}
+
+describe('libp2p-webtransport', () => {
+  it('webtransport connects to go-libp2p', async () => {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const maStr: string = process.env.serverAddr!
+    const ma = multiaddr(maStr)
+    const node = await createLibp2p({
+      transports: [webTransport()],
+      connectionEncryption: [noise()]
+    })
+
+    await node.start()
+
+    // Ping many times
+    for (let index = 0; index < 100; index++) {
+      const now = Date.now()
+
+      // Note we're re-implementing the ping protocol here because as of this
+      // writing, go-libp2p will reset the stream instead of close it. The next
+      // version of go-libp2p v0.24.0 will have this fix. When that's released
+      // we can use the builtin ping system
+      const stream = await node.dialProtocol(ma, '/ipfs/ping/1.0.0')
+
+      const data = new Uint8Array(32)
+      globalThis.crypto.getRandomValues(data)
+
+      const pong = new Promise<void>((resolve, reject) => {
+        (async () => {
+          for await (const chunk of stream.source) {
+            const v = chunk.subarray()
+            const byteMatches: boolean = v.every((byte: number, i: number) => byte === data[i])
+            if (byteMatches) {
+              resolve()
+            } else {
+              reject(new Error('Wrong pong'))
+            }
+          }
+        })().catch(reject)
+      })
+
+      let res = -1
+      await stream.sink((async function * () {
+        yield data
+        // Wait for the pong before we close the write side
+        await pong
+        res = Date.now() - now
+      })())
+
+      await stream.close()
+
+      expect(res).to.be.greaterThan(-1)
+    }
+
+    await node.stop()
+    const conns = node.getConnections()
+    expect(conns.length).to.equal(0)
+  })
+
+  it('fails to connect without certhashes', async () => {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const maStr: string = process.env.serverAddr!
+    const maStrNoCerthash: string = maStr.split('/certhash')[0]
+    const maStrP2p = maStr.split('/p2p/')[1]
+    const ma = multiaddr(maStrNoCerthash + '/p2p/' + maStrP2p)
+
+    const node = await createLibp2p({
+      transports: [webTransport()],
+      connectionEncryption: [noise()]
+    })
+    await node.start()
+
+    const err = await expect(node.dial(ma)).to.eventually.be.rejected()
+    expect(err.errors[0].toString()).to.contain('Expected multiaddr to contain certhashes')
+
+    await node.stop()
+  })
+
+  it('Closes writes of streams after they have sunk a source', async () => {
+    // This is the behavor of stream muxers: (see mplex, yamux and compliance tests: https://github.com/libp2p/js-libp2p-interfaces/blob/master/packages/interface-stream-muxer-compliance-tests/src/close-test.ts)
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const maStr: string = process.env.serverAddr!
+    const ma = multiaddr(maStr)
+    const node = await createLibp2p({
+      transports: [webTransport()],
+      connectionEncryption: [noise()]
+    })
+
+    async function * gen () {
+      yield new Uint8Array([0])
+      yield new Uint8Array([1, 2, 3, 4])
+      yield new Uint8Array([5, 6, 7])
+      yield new Uint8Array([8, 9, 10, 11])
+      yield new Uint8Array([12, 13, 14, 15])
+    }
+
+    await node.start()
+    const stream = await node.dialProtocol(ma, 'echo')
+
+    await stream.sink(gen())
+
+    let expectedNextNumber = 0
+    for await (const chunk of stream.source) {
+      for (const byte of chunk.subarray()) {
+        expect(byte).to.equal(expectedNextNumber++)
+      }
+    }
+    expect(expectedNextNumber).to.equal(16)
+
+    // Close read, we've should have closed the write side during sink
+    stream.closeRead()
+
+    expect(stream.stat.timeline.close).to.be.greaterThan(0)
+
+    await node.stop()
+  })
+})
+
+describe('test helpers', () => {
+  it('correctly checks subsets', () => {
+    const testCases = [
+      { a: [[1, 2, 3]], b: [[4, 5, 6]], isSubset: false },
+      { a: [[1, 2, 3], [4, 5, 6]], b: [[1, 2, 3]], isSubset: true },
+      { a: [[1, 2, 3], [4, 5, 6]], b: [], isSubset: true },
+      { a: [], b: [[1, 2, 3]], isSubset: false },
+      { a: [], b: [], isSubset: true },
+      { a: [[1, 2, 3]], b: [[1, 2, 3], [4, 5, 6]], isSubset: false },
+      { a: [[1, 2, 3]], b: [[1, 2]], isSubset: false }
+    ]
+
+    for (const tc of testCases) {
+      expect(isSubset(tc.a.map(b => new Uint8Array(b)), tc.b.map(b => new Uint8Array(b)))).to.equal(tc.isSubset)
+    }
+  })
+})
